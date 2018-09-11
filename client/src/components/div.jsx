e06eb0e34034a5657ac635a6d0382fdfc64ba82e
Yon Taek Chung <yontaekc@gwu.edu>
2018-09-11 09:56:54 -0700

    added button functionality

--- client/src/components/div.jsx
+++ client/src/components/div.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-var WhatIsThis = ({ restaurant }) => {
+var WhatIsThis = ({ restaurant, onLeftClick, onRightClick, index }) => {
   return (
     <div className="container">
       <div className="testing">
@@ -11,11 +11,14 @@ var WhatIsThis = ({ restaurant }) => {
             viewBox="0 0 24 24"
             width="24"
             xmlns="http://www.w3.org/2000/svg"
+            onClick={() => {
+              onLeftClick(restaurant);
+            }}
           >
             <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
             <path d="M0 0h24v24H0z" fill="none" />
           </svg>
-          <img className="image" src={restaurant.picture[0]} />
+          <img className="image" src={restaurant.picture[index]} />
           <svg
             fill="#000000"
             height="24"
@@ -23,6 +26,9 @@ var WhatIsThis = ({ restaurant }) => {
             width="24"
             xmlns="http://www.w3.org/2000/svg"
             className="right-button"
+            onClick={() => {
+              onRightClick(restaurant);
+            }}
           >
             <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
             <path d="M0 0h24v24H0z" fill="none" />
